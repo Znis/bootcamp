cbc4518f35974cb889f1245e9e81948a812f5808
Jenish Twayana <57634264+Znis@users.noreply.github.com>
2024-02-23 14:25:41 +0545

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,6 @@ document.getElementById('salaryForm').addEventListener('submit', function(e) {
     e.preventDefault();
     var yearsOfExperience = parseFloat(document.getElementById('yearsOfExperience').value);
     if (!isNaN(yearsOfExperience)) {
-      var predictedSalary =  predictSalary(yearsOfExperience);
-      document.getElementById('result').innerHTML = `Predicted Salary: Rs. ${predictedSalary.toFixed(2)}`;
-    } else {
-      document.getElementById('result').innerHTML = 'Please enter a valid number for years of experience.';
-    }
-  });
-  
-  function predictSalary(yearsOfExperience) {
-   var result = 0.0;
         fetch('https://bootcamp-03yf.onrender.com/predict-salary/', {
             method: 'POST',
             headers: {
@@ -20,9 +11,19 @@ document.getElementById('salaryForm').addEventListener('submit', function(e) {
         })
         .then(response => response.json())
         .then(data => {
-            result = data.salary
+            document.getElementById('result').innerHTML = `Predicted Salary: Rs. ${data.salary.toFixed(2)}`;
         })
         .catch(error => console.error('Error:', error));
+      
+      
+    } else {
+      document.getElementById('result').innerHTML = 'Please enter a valid number for years of experience.';
+    }
+  });
+  
+  function predictSalary(yearsOfExperience) {
+   var result = 0.0;
+        
     return result
   }
   
