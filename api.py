904ffffb6a18b5108afdb1f91051dc5ea42858bc
Jenish Twayana <57634264+Znis@users.noreply.github.com>
2024-02-23 16:01:08 +0545

    Update api.py

--- api.py
+++ api.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 from joblib import load
 
 app = FastAPI()
 
+class data(BaseModel):
+    years_of_experience: float
+    
 # Allowing CORS for frontend access
 app.add_middleware(
     CORSMiddleware,
@@ -19,13 +23,13 @@ best_model = load("salary_hike_best_lr_model.pkl")
 
 
 @app.post("/predict-salary/")
-async def predict_salary(years_of_experience: float):
-    years_of_exp_input = years_of_experience
+async def predict_salary(years_of_experience:data):
+
+    years_of_exp_input = years_of_experience.years_of_experience
 
     # model prediction
     try:
         predicted_salary_linear = best_model.predict([[years_of_exp_input]])
         return {"salary": round(predicted_salary_linear[0],2)}
     except:
-        return {"salary" : "0"}
-    
+        return {"salary": "0"}
