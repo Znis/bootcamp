18298968ac75873a947e2ed8433ead45904feb3c
Jenish Twayana <57634264+Znis@users.noreply.github.com>
2024-02-23 15:59:58 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
 document.getElementById('salaryForm').addEventListener('submit', function(e) {
     e.preventDefault();
     var yearsOfExperience = parseFloat(document.getElementById('yearsOfExperience').value);
+    
     if (!isNaN(yearsOfExperience)) {
-        fetch('https://bootcamp-03yf.onrender.com/predict-salary/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ years_of_experience: yearsOfExperience })
-        })
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('result').innerHTML = `Predicted Salary: Rs. ${data.salary.toFixed(2)}`;
-        })
-        .catch(error => console.error('Error:', error));
+      const data = {
+        "years_of_experience": yearsOfExperience
+      };
       
-      
-    } else {
+      fetch('https://bootcamp-03yf.onrender.com/predict-salary/', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+      })
+      .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+          return response.json();
+      })
+      .then(result => {
+          document.getElementById('result').innerHTML = `Predicted Salary: Rs. ${result.salary}`;
+      })
+      .catch(error => {
+          console.error('Error:', error);
+          document.getElementById('result').innerHTML = 'An error occurred while processing your request.';
+      });
+  } else {
       document.getElementById('result').innerHTML = 'Please enter a valid number for years of experience.';
-    }
-  });
-  
-  function predictSalary(yearsOfExperience) {
-   var result = 0.0;
-        
-    return result
   }
+});
+      
+ 
   
